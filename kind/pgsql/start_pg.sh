e564d623b19e2c36c4bc8b10252e84b265646a9d
JC Menchi <jean.menchi@gmail.com>
2021-05-30 22:08:15 +0200

    Add shell to create needed services

--- kind/pgsql/start_pg.sh
+++ kind/pgsql/start_pg.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PG_ADMIN_USER=${PG_ADMIN_USER:-pgr}
+export PG_ADMIN_PASSWORD=${PG_ADMIN_PASSWORD:-pgr}
+
 # check if db exist
 if [ ! -d /var/lib/postgresql/data ]
 then
@@ -13,7 +16,7 @@ then
 	echo "listen_addresses='*'" >> /var/lib/postgresql/data/postgresql.conf
 
 	pg_ctl -D/var/lib/postgresql/data start
-	psql --command "CREATE USER pgr WITH CREATEDB CREATEROLE PASSWORD 'pgr';"
+	psql --command "CREATE USER ${PG_ADMIN_USER} WITH CREATEDB CREATEROLE PASSWORD '${PG_ADMIN_PASSWORD}';"
 	psql --command "CREATE USER repuser WITH REPLICATION PASSWORD 'repuser';"
 	pg_ctl -D/var/lib/postgresql/data stop
 fi
